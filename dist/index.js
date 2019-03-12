c8790d88c8e0c3ad77f68e5a64c32d369313f259
shibin-you <709594315@qq.com>
2019-03-12 13:59:40 +0800

    add method use

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = exports.required = exports.convert = exports.controller = exports.all = exports.del = exports.put = exports.post = exports.get = undefined;
+exports.default = exports.required = exports.convert = exports.controller = exports.all = exports.use = exports.del = exports.put = exports.post = exports.get = undefined;
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
@@ -44,7 +44,14 @@ var put = exports.put = function put(path) {
 
 var del = exports.del = function del(path) {
   return router({
-    method: 'del',
+    method: 'delete',
+    path: path
+  });
+};
+
+var use = exports.use = function use(path) {
+  return router({
+    method: 'use',
     path: path
   });
 };
